ca2aa7e1723856e403d2bc4e9628df0e5068dde0
reginald160 <ozougwuifeanyi160@gmail.com>
2020-08-04 11:52:53 +0100

    Added User Login icon

---
+++ SDSDWorkOrder.DataAccess/Migrations/20200804104734_UserIcon.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SDSDWorkOrder.DataAccess.Migrations
+{
+    public partial class UserIcon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
